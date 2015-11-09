74db19e279921692afdb5cfd16e2cd5d7758185f
AKSpan <alex-kot-spb@mail.ru>
2015-11-09 22:30:49 +0300

    Доработки

--- js/start.js
+++ js/start.js
@@ -8,32 +8,4 @@ window.Phonebook = {
     Router: {}
 };
 
-/*$(document).ready(function () {
-    $("#test").click(function () {
-        var dt = {
-            owner: "Span",
-            name: "Алексей",
-            surname: "Kатаев",
-            thirdname: "Яковлевич",
-            birthday: "10.01.1991",
-            phone: "+79118158639",
-            avatar: "lol.jpg",
-            social: {
-                vk: "vk.com",
-                instagram: "insta.com"
-            }
-        };
-        console.log("Yo");
-        $.ajax({//create an ajax request to load_page.php
-            url: "/mongo",
-            type: "POST",
-            data: JSON.stringify(dt),
-            success: function (data) {
-                console.log("S = ", data)
-            },
-            error: function (data) {
-                console.log("e = ", data)
-            }
-        });
-    })
-});*/
+
