c64ece955d971480bbc6ec611b540cf9dbbb8f1f
AKSpan <alex-kot-spb@mail.ru>
2015-11-28 16:02:49 +0300

    Работа над списком контактов

--- js/Views/list-view.js
+++ js/Views/list-view.js
@@ -5,36 +5,40 @@ Phonebook.Views.List = Backbone.View.extend({
     el: '#contacts',
     list_contact_template: '',
     contact_notes_template: '',
-    collection:null,
+    collection: null,
     initialize: function (collection) {
-        //console.log("init LIST collection",collection)
         this.list_contact_template = _.template($('#list-template').html());
+        this.contact_notes_template = $('#contact-note-template').html();
         this.renderListContact(collection);
     },
     renderListContact: function (collection) {
         var that = this;
-        //console.log("collection",collection)
-        _.each(collection.models,function(record){
-           that.renderOneContact(record);
+        _.each(collection.models, function (record) {
+            that.renderOneContact(record);
         });
 
     },
     renderOneContact: function (contact) {
-        this.$el.append(this.contact_notes_template(contact.toJSON()));
+        console.log(contact.toJSON())
+       this.$el.html(this.list_contact_template(contact.toJSON()));
+       //_.template(this.contact_notes_template,contact.toJSON());
+
+        // this.$el.append(_.template(this.contact_notes_template,contact.toJSON()));
+        //  console.log(this.$el);
     }
 });
 Phonebook.Views.LoadList = Backbone.View.extend({
     url_name: 'list',
     el: '#content',
     model: null,
-    collection:null,
+    collection: null,
     initialize: function (search) {
         var that = this;
         this.collection = new Phonebook.Collections.Contacts();
         this.collection.fetch({
-            data:search,
-            type:'post',
-            async:false,
+            data: search,
+            type: 'post',
+            async: false,
             success: function () {
                 that.render();
             }
